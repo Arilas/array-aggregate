0d1c67341aac8a9be2043f9a288423b31a68fe2e
Alex Grand <alex@granted.name>
2018-11-01 00:50:07 +0200

    even more tests for nullable sorting

--- src/sort/buildSort.test.js
+++ src/sort/buildSort.test.js
@@ -23,62 +23,187 @@ const makeRandomItem = () => {
     name: randomName(),
     qty: Math.ceil(Math.random() * 1000),
     date: randomDate(),
+    nullableNumber:
+      Math.random() >= 0.5 ? Math.ceil(Math.random() * 1000) : null,
+    nullableString: Math.random() >= 0.5 ? randomName() : null,
+    nullableDate: Math.random() >= 0.5 ? randomDate() : null,
   }
-  if (Math.random() > 0.5) {
-    data.nullable = null
-  } else {
-    data.nullable = randomName()
-  }
+
   return data
 }
+
+function mapValue<T: Object, N: string>(
+  name: N,
+): (item: T) => $ElementType<T, N> {
+  return item => item[name]
+}
+
 const data = Array.from(new Uint8Array(8).map(index => index)).map(
   makeRandomItem,
 )
 
-test('sort numbers ASC', () => {
-  const sort = buildSort({
-    qty: 1,
+describe('numbers', () => {
+  test('sort ASC', () => {
+    const sort = buildSort({
+      qty: 1,
+    })
+    const sorted = data.slice().sort(sort)
+    sorted.map(mapValue('qty')).reduce((previous, current) => {
+      if (previous != null && current != null) {
+        expect(previous).toBeLessThan(current)
+      }
+      return current
+    })
   })
-  const sorted = data.slice().sort(sort)
-  expect(sorted[0].qty).toBeLessThan(sorted[sorted.length - 1].qty)
+
+  test('sort DESC', () => {
+    const sort = buildSort({
+      qty: -1,
+    })
+    const sorted = data.slice().sort(sort)
+    sorted.map(mapValue('qty')).reduce((previous, current) => {
+      if (previous != null && current != null) {
+        expect(previous).toBeGreaterThan(current)
+      }
+      return current
+    })
+  })
+
+  test('sort nullable ASC', () => {
+    const sort = buildSort({
+      nullableNumber: 1,
+    })
+    const sorted = data.slice().sort(sort)
+    expect(sorted[0].nullableNumber).toBeNull()
+    sorted.map(mapValue('nullableNumber')).reduce((previous, current) => {
+      if (previous != null && current != null) {
+        expect(previous).toBeLessThan(current)
+      }
+      return current
+    })
+    expect(sorted[sorted.length - 1].nullableNumber).not.toBeNull()
+  })
+
+  test('sort nullable DESC', () => {
+    const sort = buildSort({
+      nullableNumber: -1,
+    })
+    const sorted = data.slice().sort(sort)
+    expect(sorted[0].nullableNumber).not.toBeNull()
+    sorted.map(mapValue('nullableNumber')).reduce((previous, current) => {
+      if (previous != null && current != null) {
+        expect(previous).toBeGreaterThan(current)
+      }
+      return current
+    })
+    expect(sorted[sorted.length - 1].nullableNumber).toBeNull()
+  })
 })
 
-test('sort numbers DESC', () => {
-  const sort = buildSort({
-    qty: -1,
+describe('string', () => {
+  test('sort ASC', () => {
+    const sort = buildSort({
+      name: 1,
+    })
+    const sorted = data.slice().sort(sort)
+    sorted.map(mapValue('name')).reduce((previous, current) => {
+      if (previous != null && current != null) {
+        expect(previous.localeCompare(current)).toBe(-1)
+      }
+      return current
+    })
   })
-  const sorted = data.slice().sort(sort)
-  expect(sorted[0].qty).toBeGreaterThan(sorted[sorted.length - 1].qty)
+
+  test('sort DESC', () => {
+    const sort = buildSort({
+      name: -1,
+    })
+    const sorted = data.slice().sort(sort)
+    sorted.map(mapValue('name')).reduce((previous, current) => {
+      if (previous != null && current != null) {
+        expect(previous.localeCompare(current)).toBe(1)
+      }
+      return current
+    })
+  })
+
+  test('sort nullable ASC', () => {
+    const sort = buildSort({
+      nullableString: 1,
+    })
+    const sorted = data.slice().sort(sort)
+    sorted.map(mapValue('nullableString')).reduce((previous, current) => {
+      if (previous != null && current != null) {
+        expect(previous.localeCompare(current)).toBe(-1)
+      }
+      return current
+    })
+  })
+
+  test('sort nullable DESC', () => {
+    const sort = buildSort({
+      nullableString: -1,
+    })
+    const sorted = data.slice().sort(sort)
+    sorted.map(mapValue('nullableString')).reduce((previous, current) => {
+      if (previous != null && current != null) {
+        expect(previous.localeCompare(current)).toBe(1)
+      }
+      return current
+    })
+  })
 })
 
-test('sort date ASC', () => {
-  const sort = buildSort({
-    date: 1,
+describe('date', () => {
+  test('sort ASC', () => {
+    const sort = buildSort({
+      date: 1,
+    })
+    const sorted = data.slice().sort(sort)
+    sorted.map(mapValue('date')).reduce((previous, current) => {
+      if (previous != null && current != null) {
+        expect(previous * 1).toBeLessThan(current * 1)
+      }
+      return current
+    })
   })
-  const sorted = data.slice().sort(sort)
-  expect(sorted[0].date * 1).toBeLessThan(sorted[sorted.length - 1].date * 1)
-})
 
-test('sort date DESC', () => {
-  const sort = buildSort({
-    date: -1,
+  test('sort DESC', () => {
+    const sort = buildSort({
+      date: -1,
+    })
+    const sorted = data.slice().sort(sort)
+    sorted.map(mapValue('date')).reduce((previous, current) => {
+      if (previous != null && current != null) {
+        expect(previous * 1).toBeGreaterThan(current * 1)
+      }
+      return current
+    })
   })
-  const sorted = data.slice().sort(sort)
-  expect(sorted[0].date * 1).toBeGreaterThan(sorted[sorted.length - 1].date * 1)
-})
 
-test('sort string ASC', () => {
-  const sort = buildSort({
-    name: 1,
+  test('sort nullable ASC', () => {
+    const sort = buildSort({
+      nullableDate: 1,
+    })
+    const sorted = data.slice().sort(sort)
+    sorted.map(mapValue('nullableDate')).reduce((previous, current) => {
+      if (previous != null && current != null) {
+        expect(previous * 1).toBeLessThan(current * 1)
+      }
+      return current
+    })
   })
-  const sorted = data.slice().sort(sort)
-  expect(sorted[0].name.localeCompare(sorted[sorted.length - 1].name)).toBe(-1)
-})
 
-test('sort string DESC', () => {
-  const sort = buildSort({
-    name: -1,
+  test('sort nullable DESC', () => {
+    const sort = buildSort({
+      nullableDate: -1,
+    })
+    const sorted = data.slice().sort(sort)
+    sorted.map(mapValue('nullableDate')).reduce((previous, current) => {
+      if (previous != null && current != null) {
+        expect(previous * 1).toBeGreaterThan(current * 1)
+      }
+      return current
+    })
   })
-  const sorted = data.slice().sort(sort)
-  expect(sorted[0].name.localeCompare(sorted[sorted.length - 1].name)).toBe(1)
 })
