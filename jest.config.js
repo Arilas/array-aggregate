0922887e2742ad28316f6f3c0393829794512ca5
Alex Grand <alex@granted.name>
2020-05-05 18:32:17 +0300

    Added codecov

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.ts',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+  ],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/*.(spec|test).ts'],
 }
